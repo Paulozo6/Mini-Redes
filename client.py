41b8ca8c280ad71825660d942fbfbf6ea7b16f66
Paulo Henrique <paulohenriquebr900@gmail.com>
2026-02-25 01:12:01 -0300

    Implementação da fase 2
    Adicionado controle de time-out, número de sequência e integração com o simulador de ruído do protocolo.py

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
 import socket
 import json
 import time
+from protocol import Segmento, Pacote, Quadro, enviar_pela_rede_ruidosa
 
 HOST = '127.0.0.1'
 PORT = 5000
+DESTINO_REAL = (HOST, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))  
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.settimeout(2.0) 
+
+seq_num = 0
 
 print("Conectado ao servidor.")
 
@@ -15,12 +19,35 @@ while True:
     
     if message.lower() == "sair":
         break
-    
-    pacote = {
+        
+    dados_app = {
         "type" : "chat",
         "sender" : "Cliente",
         "message" : message,
         "timestamp" : time.time()
     }
     
-    client.send(json.dumps(pacote).encode())
+    seg = Segmento(seq_num, is_ack = False, payload = dados_app)
+    pac = Pacote(src_vip="CLI", dst_vip = "SRV", ttl = 64, segmento_dict = seg.to_dict())
+    qua = Quadro(src_mac = "00:11", dst_mac = "22:33", pacote_dict = pac.to_dict())
+    
+    bytes_enviar = qua.serializar()
+    
+    ack_recebido = False
+    while not ack_recebido:
+        print(f"Enviando Seq {seq_num}...")
+        enviar_pela_rede_ruidosa(client, bytes_enviar, DESTINO_REAL)
+        
+        try:
+            dados, _ = client.recvfrom(4096)
+            quadro_dict, integro = Quadro.deserializar(dados)
+            
+            if integro:
+                segmento = quadro_dict['data']['data']
+                
+                if segmento['is_ack'] and segmento['seq_num'] == seq_num:
+                    print('ACK recebido!')
+                    ack_recebido = True
+                    seq_num = 1 - seq_num
+        except socket.timeout:
+            print("Timeout! Retransmitindo....")
--- server.py
+++ server.py
@@ -1,24 +1,44 @@
 import socket
 import json
 from datetime import datetime
+from protocol import Segmento, Pacote, Quadro, enviar_pela_rede_ruidosa
 
 HOST = '127.0.0.1'
 PORT = 5000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.bind((HOST, PORT))
-server.listen()
 
-print("Servidor ligando...")
-conn, adr = server.accept()
-print("Cliente conectado:", adr)
+print("Servidor ligano. Aguardando mensagens...")
+
+ultimo_seq_recebido = 1
 
 while True:
-    data = conn.recv(1024)
-    if not data:
-        break
+    dados_brutos, addr_cliente = server.recvfrom(4096)
+    quadro_dict, integro = Quadro.deserializar(dados_brutos)
     
-    mensagem = json.loads(data.decode())
+    if not integro: 
+        print("(ERRO DE CRC) Quadro corrompido recebido. Ignorando...")
+        continue
     
-    hora = datetime.fromtimestamp(mensagem['timestamp']).strftime('%H:%M:%S')
-    print(f"[{mensagem['sender']} - {hora}] {mensagem['message']}")
+    pacote_dict = quadro_dict['data']
+    segmento_dict = pacote_dict['data']
+    payload = segmento_dict['payload']
+    seq_num = segmento_dict['seq_num']
+    
+    print(f"Recebido Seq {seq_num}. Enviando ACK...")
+
+    ack_seg = Segmento(seq_num, is_ack = True, payload = None)
+    ack_pac = Pacote(src_vip = "SERVIDOR", dst_vip = "CLIENTE", ttl = 64, segmento_dict = ack_seg.to_dict())
+    ack_qua = Quadro(src_mac = "22:33", dst_mac = "00:11", pacote_dict = ack_pac.to_dict())
+    
+    bytes_enviar = ack_qua.serializar()
+    enviar_pela_rede_ruidosa(server, bytes_enviar, addr_cliente)
+    
+    if seq_num != ultimo_seq_recebido:
+        print("Nova mensagem recebida:")
+        hora = datetime.now().strftime('%H:%M:%S')
+        print(f"[{payload['sender']} - {hora}] {payload['message']}")
+        ultimo_seq_recebido = seq_num
+    else:
+        print("Mensagem duplicada recebida. Ignorando conteúdo.")
